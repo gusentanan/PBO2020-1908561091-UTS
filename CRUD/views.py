fb107afffc40575c083e0f231efddea5b6b05b67
Bagus Merta <merta.bagus24@gmail.com>
2020-11-16 03:08:17 +0800

    done editing

--- CRUD/views.py
+++ CRUD/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView, DeleteView
-from django.urls import reverse_lazy
+
 
 from .forms import StudentForm
 from .models import Student
