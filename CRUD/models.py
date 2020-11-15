9e0fcba1c8f4f21a6026eb1cb4eef1396c00ae9c
Bagus Merta <merta.bagus24@gmail.com>
2020-11-16 03:07:08 +0800

    clean

--- CRUD/models.py
+++ CRUD/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Student(models.Model):
 	name = models.CharField(max_length=40, null=True)
 	number = models.CharField(max_length=40, null=True)
+	
+	def __str__(self):
+		return self.name
 
 
